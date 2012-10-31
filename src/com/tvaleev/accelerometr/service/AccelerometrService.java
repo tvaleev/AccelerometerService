37ce7ab3895487e625119e8ae9a7c3de3fc215b3
Timur Valeev <tvaleev@gmail.com>
2012-11-01 02:57:10 +0400

    Added project main files

---
+++ src/com/tvaleev/accelerometr/service/AccelerometrService.java
@@ -0,0 +1,84 @@
+package com.tvaleev.accelerometr.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class AccelerometrService extends Service implements SensorEventListener {
+
+	private SensorManager sensorManager;
+	private final IBinder mBinder = new AccBinder();
+	
+	private float DataFromAccelerometr;
+	
+	public float getDataFromAccelerometr() {
+		return DataFromAccelerometr;
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return mBinder;
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onCreate() {
+		Toast.makeText(this, "Service is created", Toast.LENGTH_SHORT).show();
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+	}
+
+	@Override
+	public void onStart(Intent intent, int startid) {
+		Log.d("[Timur]","calling onStart");
+		Toast.makeText(this, "Service is run", Toast.LENGTH_SHORT).show();
+		sensorManager.registerListener(this,
+				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+				SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	public void onDestroy() {
+		Log.d("[Timur]","calling onDestroy");
+		Toast.makeText(this, "Service is stopped", Toast.LENGTH_SHORT).show();
+		sensorManager.unregisterListener(this);
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			getAccelerometer(event);
+		}
+	}
+
+	private void getAccelerometer(SensorEvent event) {
+		float[] values = event.values;
+
+		float x = values[0];
+		float y = values[1];
+		float z = values[2];
+
+		float accelationSquareRoot = (x * x + y * y + z * z)
+				/ (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+		
+		DataFromAccelerometr = accelationSquareRoot;
+	}
+
+	public class AccBinder extends Binder {
+		AccelerometrService getService() {
+			return AccelerometrService.this;
+		}
+	}
+
+}
---
+++ src/com/tvaleev/accelerometr/service/MainActivity.java
@@ -0,0 +1,76 @@
+package com.tvaleev.accelerometr.service;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	private AccelerometrService s;
+
+	private ServiceConnection mConnection = new ServiceConnection() {
+
+		public void onServiceConnected(ComponentName className, IBinder binder) {
+			s = ((AccelerometrService.AccBinder) binder).getService();
+			Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT)
+					.show();
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			s = null;
+		}
+	};
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		final Button btnStart = (Button) findViewById(R.id.btn_start);
+		final Button btnStop = (Button) findViewById(R.id.btn_stop);
+		final TextView accData = (TextView) findViewById(R.id.acc_text_data);
+
+		btnStart.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startService(new Intent(MainActivity.this,
+						AccelerometrService.class));
+				if (s != null) {
+					accData.setText(Float.toString(s.getDataFromAccelerometr()));
+				}
+			}
+		});
+
+		btnStop.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				stopService(new Intent(MainActivity.this,
+						AccelerometrService.class));
+				accData.setText("Stopped");
+			}
+		});
+
+		doBindService();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	void doBindService() {
+		bindService(new Intent(this, AccelerometrService.class), mConnection,
+				Context.BIND_AUTO_CREATE);
+	}
+}
